89f19fea65ff450b14b4ad4a13da2c3ba9132c99
JKim <ousiarche@gmail.com>
2026-01-14 11:39:42 +0900

    Fix scaffold padding parameter not being used

--- app/src/main/java/com/ousiarche/todo/MainActivity.kt
+++ app/src/main/java/com/ousiarche/todo/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import com.ousiarche.todo.frame.BottomSheet
 import com.ousiarche.todo.frame.FloatingButton
 import com.ousiarche.todo.ui.theme.TodoTheme
@@ -26,12 +29,18 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     floatingActionButton = { FloatingButton { scope.launch { sheetState.show() }} },
                 ) {
-                    if (sheetState.isVisible) {
-                        BottomSheet(
-                            sheetState = sheetState,
-                            onDismissRequest = { scope.launch { sheetState.hide() } }
-                        )
-                    }
+                    innerPadding ->
+                        Box(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                        ) {
+                            if (sheetState.isVisible) {
+                                BottomSheet(
+                                    sheetState = sheetState,
+                                    onDismissRequest = { scope.launch { sheetState.hide() } },
+                                )
+                            }
+                        }
                 }
             }
         }
